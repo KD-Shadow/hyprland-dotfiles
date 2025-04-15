7b4d8b3b1cc421ee446ba5c1e27a4af96e7ba705
KD-Shadow <kdshadow189@gmail.com>
2025-04-15 09:57:20 +0545

    first one

---
+++ cmus/.config/cmus/lib.pl
@@ -0,0 +1,129 @@
+/home/sh4dow/Music/Best Songs/24-7-365.mp3
+/home/sh4dow/Music/Best Songs/8-letters-why-don't-we.mp3
+/home/sh4dow/Music/Best Songs/aaj-sajeya.mp3
+/home/sh4dow/Music/Best Songs/aakhale.mp3
+/home/sh4dow/Music/Best Songs/all we know.mp3
+/home/sh4dow/Music/Best Songs/anyone else.mp3
+/home/sh4dow/Music/Best Songs/atteri.mp3
+/home/sh4dow/Music/Best Songs/aziyat.mp3
+/home/sh4dow/Music/Best Songs/bad liar.mp3
+/home/sh4dow/Music/Best Songs/bad.mp3
+/home/sh4dow/Music/Best Songs/baimani.mp3
+/home/sh4dow/Music/Best Songs/be alright.mp3
+/home/sh4dow/Music/Best Songs/be kind.mp3
+/home/sh4dow/Music/Best Songs/bez-tebja-ja-ne-ja.mp3
+/home/sh4dow/Music/Best Songs/broken.mp3
+/home/sh4dow/Music/Best Songs/can-we-kiss-forever.mp3
+/home/sh4dow/Music/Best Songs/cry-with-you.mp3
+/home/sh4dow/Music/Best Songs/dancing-with-your-ghost.mp3
+/home/sh4dow/Music/Best Songs/death bead.mp3
+/home/sh4dow/Music/Best Songs/dhairya.mp3
+/home/sh4dow/Music/Best Songs/ditto.mp3
+/home/sh4dow/Music/Best Songs/dont go.mp3
+/home/sh4dow/Music/Best Songs/duniyaa.mp3
+/home/sh4dow/Music/Best Songs/dynasty.mp3
+/home/sh4dow/Music/Best Songs/dzanum.mp3
+/home/sh4dow/Music/Best Songs/ek mauka.mp3
+/home/sh4dow/Music/Best Songs/everyone changes.mp3
+/home/sh4dow/Music/Best Songs/finding-hope-3-00.mp3
+/home/sh4dow/Music/Best Songs/fingers-crossed.mp3
+/home/sh4dow/Music/Best Songs/fly.mp3
+/home/sh4dow/Music/Best Songs/follow me.mp3
+/home/sh4dow/Music/Best Songs/follow u everywhere.mp3
+/home/sh4dow/Music/Best Songs/fuck love.mp3
+/home/sh4dow/Music/Best Songs/gajalu.mp3
+/home/sh4dow/Music/Best Songs/ghost.mp3
+/home/sh4dow/Music/Best Songs/girl-of-my-dreams.mp3
+/home/sh4dow/Music/Best Songs/give-me-your-forever.mp3
+/home/sh4dow/Music/Best Songs/golden-hour.mp3
+/home/sh4dow/Music/Best Songs/got-me-thinking.mp3
+/home/sh4dow/Music/Best Songs/happy-tears.mp3
+/home/sh4dow/Music/Best Songs/harayeko-maya.mp3
+/home/sh4dow/Music/Best Songs/hawa-le.mp3
+/home/sh4dow/Music/Best Songs/heat waves.mp3
+/home/sh4dow/Music/Best Songs/heer-ranjha.mp3
+/home/sh4dow/Music/Best Songs/high high high.mp3
+/home/sh4dow/Music/Best Songs/hold-on.mp3
+/home/sh4dow/Music/Best Songs/house-with-no-mirrors.mp3
+/home/sh4dow/Music/Best Songs/humnava-mere.mp3
+/home/sh4dow/Music/Best Songs/hyatteri.mp3
+/home/sh4dow/Music/Best Songs/i can't hate you.mp3
+/home/sh4dow/Music/Best Songs/i-found-her.mp3
+/home/sh4dow/Music/Best Songs/i liked you.mp3
+/home/sh4dow/Music/Best Songs/i need you.mp3
+/home/sh4dow/Music/Best Songs/its raining its pouring.mp3
+/home/sh4dow/Music/Best Songs/jaan ban gaye.mp3
+/home/sh4dow/Music/Best Songs/juneli.mp3
+/home/sh4dow/Music/Best Songs/k-garu-timrai-huna-lai-mantramugdha.mp3
+/home/sh4dow/Music/Best Songs/khoj.mp3
+/home/sh4dow/Music/Best Songs/kina-timro-huna-sakina.mp3
+/home/sh4dow/Music/Best Songs/koina uta.mp3
+/home/sh4dow/Music/Best Songs/last girl.mp3
+/home/sh4dow/Music/Best Songs/let me down slowly.mp3
+/home/sh4dow/Music/Best Songs/light switch.mp3
+/home/sh4dow/Music/Best Songs/little bit better.mp3
+/home/sh4dow/Music/Best Songs/make-us-never-happen.mp3
+/home/sh4dow/Music/Best Songs/maya.mp3
+/home/sh4dow/Music/Best Songs/meant-to-be.mp3
+/home/sh4dow/Music/Best Songs/middle-of-the-night.mp3
+/home/sh4dow/Music/Best Songs/miss you more.mp3
+/home/sh4dow/Music/Best Songs/nayan.mp3
+/home/sh4dow/Music/Best Songs/on my mind.mp3
+/home/sh4dow/Music/Best Songs/pal.mp3
+/home/sh4dow/Music/Best Songs/parkha-hai-maya.mp3
+/home/sh4dow/Music/Best Songs/pheli dafa.mp3
+/home/sh4dow/Music/Best Songs/point of view.mp3
+/home/sh4dow/Music/Best Songs/ranjha.mp3
+/home/sh4dow/Music/Best Songs/sad kids.mp3
+/home/sh4dow/Music/Best Songs/sakhiyaan.mp3
+/home/sh4dow/Music/Best Songs/sarangi.mp3
+/home/sh4dow/Music/Best Songs/sasto mutu.mp3
+/home/sh4dow/Music/Best Songs/save a life.mp3
+/home/sh4dow/Music/Best Songs/save me.mp3
+/home/sh4dow/Music/Best Songs/scars-to-your-beautiful.mp3
+/home/sh4dow/Music/Best Songs/seasons.mp3
+/home/sh4dow/Music/Best Songs/senbonzakura.mp3
+/home/sh4dow/Music/Best Songs/serendipity.mp3
+/home/sh4dow/Music/Best Songs/setsuna no chikai.mp3
+/home/sh4dow/Music/Best Songs/shame.mp3
+/home/sh4dow/Music/Best Songs/shayad.mp3
+/home/sh4dow/Music/Best Songs/Shelter.mp3
+/home/sh4dow/Music/Best Songs/shirshak.mp3
+/home/sh4dow/Music/Best Songs/standing by you.mp3
+/home/sh4dow/Music/Best Songs/stone cold.mp3
+/home/sh4dow/Music/Best Songs/sweet things.mp3
+/home/sh4dow/Music/Best Songs/tell me about u.mp3
+/home/sh4dow/Music/Best Songs/tere alawa.mp3
+/home/sh4dow/Music/Best Songs/thats just life.mp3
+/home/sh4dow/Music/Best Songs/timro-pratiksa.mp3
+/home/sh4dow/Music/Best Songs/tu-chodiyon-na.mp3
+/home/sh4dow/Music/Best Songs/turn back time.mp3
+/home/sh4dow/Music/Best Songs/two hearts.mp3
+/home/sh4dow/Music/Best Songs/unmiss u.mp3
+/home/sh4dow/Music/Best Songs/water fountain.mp3
+/home/sh4dow/Music/Best Songs/way back home.mp3
+/home/sh4dow/Music/Best Songs/where-we-re-going.mp3
+/home/sh4dow/Music/Best Songs/where we started.mp3
+/home/sh4dow/Music/Best Songs/willow tree.mp3
+/home/sh4dow/Music/Best Songs/wish-i-didn-t-know-you.mp3
+/home/sh4dow/Music/Best Songs/wish-you-pain.mp3
+/home/sh4dow/Music/Best Songs/with you.mp3
+/home/sh4dow/Music/Best Songs/working for u.mp3
+/home/sh4dow/Music/Best Songs/yaeow-i-m-just-a-ghost.mp3
+/home/sh4dow/Music/Best Songs/yaeow-the-thing-about-you.mp3
+/home/sh4dow/Music/Best Songs/yenti-yenti.mp3
+/home/sh4dow/Music/Best Songs/you-broke-me-first.mp3
+/home/sh4dow/Music/Best Songs/you-can-go-and-be-with-him.mp3
+/home/sh4dow/Music/Best Songs/youre my moon.mp3
+/home/sh4dow/Music/Best Songs/your eyes.mp3
+/home/sh4dow/Music/Best Songs/your sweater.mp3
+/home/sh4dow/Music/Best Songs/yume-to-hazakura.mp3
+/home/sh4dow/Music/Lofi/4-a-m-study-session-lofi-hip-hop_audio_super.mp3
+/home/sh4dow/Music/Lofi/a-finding-hope-mix-sad-music-playlist_audio_super.mp3
+/home/sh4dow/Music/Lofi/an-unfinished-song-cover-by-lime-wuthering-waves_audio_super.mp3
+/home/sh4dow/Music/Lofi/cadmium-click-scroll-feat-eli-raain-veronica-bravo-lyric-video_audio_super.mp3
+/home/sh4dow/Music/Lofi/finding-hope-1-hour-mix-sad-music-playlist_audio_super.mp3
+/home/sh4dow/Music/Lofi/nightcore-top-30-songs-of-thefatrat-2024-thefatrat-mega-mix-best-of-thefatrat_audio_super.mp3
+/home/sh4dow/Music/Lofi/playlist-vibe-songs-that-i-sure-100-feel-good_audio_super.mp3
+/home/sh4dow/Music/Lofi/sad-songs-for-sad-people-yaeow-sad-music-mix_audio_super.mp3
+/home/sh4dow/Music/Lofi/shiloh-dynasty-vibes-sad-music-mix_audio_super.mp3
