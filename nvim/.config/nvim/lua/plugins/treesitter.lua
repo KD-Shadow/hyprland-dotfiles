924c517ade8ff9e6246b0fba1169ecdd36769139
Shadow <kdshadow189@gmail.com>
2025-04-15 16:22:00 +0545

    new

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,9 +4,17 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
+			ensure_installed = {
+				"c",
+				"cpp",
+				"lua", -- optional: add languages you use often
+				"vim",
+				"bash",
+			},
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
 	end,
 }
+
